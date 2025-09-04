186c37fea75e5fdf1d7f9ca0f77258465f40727d
BOUAMRANI Fahed <fahed.bouamrani@etu.univ-orleans.fr>
2025-09-04 10:10:55 +0200

    ajout de la page home avec parametre + ajout de la page home sans parametre routé

--- tutoDjango/monApp/urls.py
+++ tutoDjango/monApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path("", views.home, name="home"),
+path("home/", views.home, name="home"),
 path("contactus", views.contactUs, name="contactus"),
-path("aboutus", views.aboutUs, name="aboutus")
+path("aboutus", views.aboutUs, name="aboutus"),
+path('home/<param>', views.home, name='home')
 ]
--- tutoDjango/monApp/views.py
+++ tutoDjango/monApp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-def home(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
+
+def home(request, param="Django"):
+    return HttpResponse(f"<h1>Hello {param} !</h1>")
 
 def contactUs(request):
     return HttpResponse("<h1>Contact Us !<h1>")
 
 def aboutUs(request):
     return HttpResponse("<h1>About Us !<h1>")
+
