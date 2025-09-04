9ff13b343444189cabe65525f43a23c6a992ae94
BOUAMRANI Fahed <fahed.bouamrani@etu.univ-orleans.fr>
2025-09-04 09:56:13 +0200

    ajout de la page contact us

--- tutoDjango/monApp/views.py
+++ tutoDjango/monApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
     return HttpResponse("<h1>Hello Django!</h1>")
+
+def contactUs(request):
+    return HttpResponse("<h1>Contact Us !<h1>")
