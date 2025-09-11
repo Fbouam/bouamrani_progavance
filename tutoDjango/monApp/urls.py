efa5655a41577ced027db63a0ac4bb6a60bc58a6
BOUAMRANI Fahed <fahed.bouamrani@etu.univ-orleans.fr>
2025-09-11 09:56:04 +0200

    j'ai fini le tp 1

--- tutoDjango/monApp/urls.py
+++ tutoDjango/monApp/urls.py
@@ -1,8 +1,14 @@
+
 from django.urls import path
 from . import views
+
 urlpatterns = [
-path("home/", views.home, name="home"),
-path("contactus", views.contactUs, name="contactus"),
-path("aboutus", views.aboutUs, name="aboutus"),
-path('home/<param>', views.home, name='home')
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('home/<param>', views.home, name='home'),
+    path('contactus', views.contactus, name='contactus'),
+    path('aboutus', views.aboutus, name='aboutus'),
+    path('produits/', views.ListProduits, name='list_produits'),
+    path('categories/', views.ListCategories, name='list_categories'),
+    path('statut/', views.ListStatuts, name='list_statut'),
 ]
--- tutoDjango/monApp/views.py
+++ tutoDjango/monApp/views.py
@@ -1,13 +1,53 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Produit, Categorie, Statut
+def home(request, param='Django'):
+    return HttpResponse(f"<h1>Bonjour {param} !</h1>")
 
-def home(request, param="Django"):
-    return HttpResponse(f"<h1>Hello {param} !</h1>")
+def contactus(request):
+    return HttpResponse('<h1>Contact Us!</h1>')
 
-def contactUs(request):
-    return HttpResponse("<h1>Contact Us !<h1>")
+def aboutus(request):
+    return HttpResponse('<h1>About Us!</h1>')
 
-def aboutUs(request):
-    return HttpResponse("<h1>About Us !<h1>")
+def ListProduits(request):
+    prdts = Produit.objects.all()
+    if not prdts:
+        html = "<h1>Liste des produits</h1><p>Aucun produit trouvé.</p>"
+    else:
+        li_items = "".join([f"<li>{p.intituleProd}</li>" for p in prdts])
+        html = f"""
+        <h1>Liste des produits</h1>
+        <ul>
+            {li_items}
+        </ul>
+        """
+    return HttpResponse(html)
 
+def ListCategories(request):
+    cats = Categorie.objects.all()
+    if not cats:
+        html = "<h1>Liste des catégories</h1><p>Aucune catégorie trouvée.</p>"
+    else:
+        li_items = "".join([f"<li>{c.nomCat}</li>" for c in cats])
+        html = f"""
+        <h1>Liste des catégories</h1>
+        <ul>
+            {li_items}
+        </ul>
+        """
+    return HttpResponse(html)
+
+def ListStatuts(request):
+    stats = Statut.objects.all()
+    if not stats:
+        html = "<h1>Liste des statuts</h1><p>Aucun statut trouvé.</p>"
+    else:
+        li_items = "".join([f"<li>{s.libelleStatut}</li>" for s in stats])
+        html = f"""
+        <h1>Liste des statuts</h1>
+        <ul>
+            {li_items}
+        </ul>
+        """
+    return HttpResponse(html)
