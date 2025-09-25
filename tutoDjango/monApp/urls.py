065e0207fd3cbd7b902e4eb0391b04a7520c23ff
BOUAMRANI Fahed <fahed.bouamrani@etu.univ-orleans.fr>
2025-09-25 10:30:23 +0200

    j'ai fini la section (La vue générique de base) et j'ai peut etre oublié de commit le debut du td

--- tutoDjango/monApp/urls.py
+++ tutoDjango/monApp/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
-    path('home/<param>', views.home, name='home'),
-    path('contactus/', views.contactus, name='contactus'),
-    path('aboutus/', views.aboutus, name='aboutus'),
+    path("home/", views.HomeView.as_view()),
+    #path('', views.home, name='home'),
+    #path('home/', views.home, name='home'),
+    #path('home/<param>', views.home, name='home'),
+    path("contactus/", views.ContactView.as_view()),
+    #path('contactus', views.contactus, name='contactus'),
+    path('aboutus/', views.AboutView.as_view()),
+    #path('aboutus', views.aboutus, name='aboutus'),
     path('produits/', views.ListProduits, name='list_produits'),
     path('categories/', views.ListCategories, name='list_categories'),
     path('statut/', views.ListStatuts, name='list_statut'),
--- tutoDjango/monApp/views.py
+++ tutoDjango/monApp/views.py
@@ -1,22 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.views.generic import TemplateView
 from .models import Produit, Categorie, Statut
 from . import models as models_module
 
-def home(request, param='Django'):
-    if request.GET and request.GET["test"]:
-        raise Http404
-    return HttpResponse("Bonjour Monde")
+class HomeView(TemplateView):
+    template_name = "monApp/page_home.html"
+
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
 
 def contactus(request):
     return render(request, 'monApp/contact.html')
 
-def aboutus(request):
-    return render(request, 'monApp/about.html')
+class ContactView(TemplateView):
+    template_name = "monApp/contact.html"
+
+    def get(self, request, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+
+class AboutView(TemplateView):
+    template_name = "monApp/page_home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context['titreh1'] = "About us..."
+        return context
+
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
 
 def ListProduits(request):
     prdts = Produit.objects.all()
-    return render(request, 'monApp/list_produits.html',{'prdts': prdts})
+    return render(request, 'monApp/list_produits.html', {'prdts': prdts})
 
 def ListCategories(request):
     cats = Categorie.objects.all()
