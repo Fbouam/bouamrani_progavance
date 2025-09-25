19e9d79194b7ba5aee2fd2a0e6dd31d051d25f87
BOUAMRANI Fahed <fahed.bouamrani@etu.univ-orleans.fr>
2025-09-25 10:59:41 +0200

    j'ai fais le fichier detail_produit.html

--- tutoDjango/monApp/urls.py
+++ tutoDjango/monApp/urls.py
@@ -6,13 +6,16 @@ urlpatterns = [
     path("home/", views.HomeView.as_view()),
     #path('', views.home, name='home'),
     #path('home/', views.home, name='home'),
-    #path('home/<param>', views.home, name='home'),
+    path('home/<param>/', views.HomeParamView.as_view()),
     path("contactus/", views.ContactView.as_view()),
     #path('contactus', views.contactus, name='contactus'),
     path('aboutus/', views.AboutView.as_view()),
     #path('aboutus', views.aboutus, name='aboutus'),
-    path('produits/', views.ListProduits, name='list_produits'),
+    #path('produits/', views.ListProduits, name='list_produits'),
+    path("produits/",views.ProduitListView.as_view()),
+    path("produit/<pk>/",views.ProduitDetailView.as_view()),
     path('categories/', views.ListCategories, name='list_categories'),
     path('statut/', views.ListStatuts, name='list_statut'),
+    
     
 ]
--- tutoDjango/monApp/views.py
+++ tutoDjango/monApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from .models import Produit, Categorie, Statut
 from . import models as models_module
 
@@ -9,18 +9,56 @@ class HomeView(TemplateView):
 
     def post(self, request, **kwargs):
         return render(request, self.template_name)
+    
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['titreh1'] = "Hello DJANGO"
+        return context
+    
+class HomeParamView(TemplateView):
+    template_name = "monApp/page_home.html"
+
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+    
+    def get_context_data(self, **kwargs):
+        context = super(HomeParamView, self).get_context_data(**kwargs)
+        param = self.kwargs.get('param')
+        context['titreh1'] = f"Hello {param}"
+        return context
+    
+
+class ProduitListView(ListView):
+    model = Produit
+    template_name = "monApp/list_produits.html"
+    context_object_name = "prdts"
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProduitListView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Liste de mes produits"
+        return context
+    
+class ProduitDetailView(DetailView):
+    model = Produit
+    template_name = "monApp/detail_produit.html"
+    context_object_name = "prdt"
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProduitDetailView, self).get_context_data(**kwargs)
+        context['titremenu'] = "Détail du produit"
+        return context
 
 def contactus(request):
     return render(request, 'monApp/contact.html')
 
 class ContactView(TemplateView):
-    template_name = "monApp/contact.html"
+    template_name = "monApp/page_home.html"
 
-    def get(self, request, **kwargs):
-        return render(request, self.template_name)
-
-    def post(self, request, **kwargs):
-        return render(request, self.template_name)
+    def get_context_data(self, **kwargs):
+        context = super(ContactView, self).get_context_data(**kwargs)
+        context['param'] = None
+        context['titreh1'] = "Contact us..."
+        return context
 
 class AboutView(TemplateView):
     template_name = "monApp/page_home.html"
